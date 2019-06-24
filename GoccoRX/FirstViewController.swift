ac385eb0ce0672149f74705eb59de0b73a1e6613
Carlos Santana <csantana7@gmail.com>
2019-06-24 11:20:23 +0100

    Initial Commit

---
+++ GoccoRX/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  GoccoRX
+//
+//  Created by Carlos Santana on 24/06/2019.
+//  Copyright © 2019 Carlos Santana. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoccoRX/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GoccoRX
+//
+//  Created by Carlos Santana on 24/06/2019.
+//  Copyright © 2019 Carlos Santana. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
